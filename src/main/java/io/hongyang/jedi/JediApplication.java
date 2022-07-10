5de0e429b162b45f544e3a69482f5ae2e5b5b2e7
hongyang <hongyangprivate@gmail.com>
2022-07-10 12:53:58 +0800

    Initial commit

---
+++ src/main/java/io/hongyang/jedi/JediApplication.java
@@ -0,0 +1,12 @@
+package io.hongyang.jedi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JediApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JediApplication.class, args);
+  }
+}
---
+++ src/test/java/io/hongyang/jedi/JediApplicationTests.java
@@ -0,0 +1,11 @@
+package io.hongyang.jedi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JediApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
